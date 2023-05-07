5cf6354646377377e5e483bf94a6a5542a0a4fbd
Karim-ElSharkawy <k_elsharkawy@hotmail.com>
2023-05-07 15:36:29 +0200

    added a test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'string-mix-web'`, () => {
+  it('should show correct output after input changes', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('string-mix-web');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    app.firstString = 'mmmmm m nnnnn y&friend&Paul has heavy hats! &';
+    app.secondString = 'my frie n d Joh n has ma n y ma n y frie n ds n&';
+    app.stringMix();
     fixture.detectChanges();
+
+    // expect correct output from stringMix.
+    expect(app.resultString).toEqual('1:mmmmmm/=nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=ee/=ss');
+
+    // expect value to be reflected correctly on webpage.
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('string-mix-web app is running!');
+    expect(compiled.querySelector('#result-string')?.textContent).toContain('1:mmmmmm/=nnnnnn/1:aaaa/1:hhh/2:yyy/2:dd/2:ff/2:ii/2:rr/=ee/=ss');
   });
 });
